5191f342c4db5f48ca33809f1d87c76a46bd8b89
1CarlosRenan <carlosrenan96@gmail.com>
2022-04-24 20:14:50 -0300

    ex-0804-destructuring

---
+++ js-es6/exercicios-js-origamid/ex-0804-destructuring.js
@@ -0,0 +1,20 @@
+// Extraia o backgroundColor, color e margin do btn
+const btn = document.querySelector('button');
+const btnStyles = getComputedStyle(btn);
+
+const {backgroundColor, color, margin} = btnStyles;
+
+// Troque os valores das variáveis abaixo
+let cursoAtivo = 'JavaScript';
+let cursoInativo = 'HTML';
+
+[cursoAtivo, cursoInativo] = ['React JS', 'WordPress como CMS']
+
+// Corrija o erro abaixo
+const cachorro = {
+  nome: 'Bob',
+  raca: 'Labrador',
+  cor: 'Amarelo'
+}
+
+const {cor: bobCor} = cachorro;
