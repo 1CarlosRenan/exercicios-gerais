7edf4f3d3f2a4e7414193e369bca5d4f8a5442d3
1CarlosRenan <carlosrenan96@gmail.com>
2022-06-17 12:18:37 -0300

    react origamid: useState

---
+++ react-origamid/0301-useState/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Produto from './Produto';
+
+const App = () => {
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
+
+  console.log('teste');
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>smartphone</button>
+      <button onClick={handleClick}>tablet</button>
+      <button onClick={handleClick}>notebook</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
+    </>
+  );
+};
+
+export default App;
