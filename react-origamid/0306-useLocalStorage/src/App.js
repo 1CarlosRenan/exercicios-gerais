514864027006a407a181176d11ad0b18365139ef
1CarlosRenan <carlosrenan96@gmail.com>
2022-06-18 13:20:49 -0300

    Custom Hooks: criando Hooks com Local Storage

---
+++ react-origamid/0306-useLocalStorage/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import useLocalStorage from './useLocalStorage';
+
+const App = () => {
+  const [produto, setProduto] = useLocalStorage('produto', '');
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  return (
+    <div>
+      <p>Preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </div>
+  );
+};
+
+export default App;
